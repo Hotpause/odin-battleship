693135b712855348ac70442ece83dd15cf16684c
Hotpause <ashutoshagarwal1006@gmail.com>
2024-04-23 12:57:15 +0530

    Editing info for new project

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        watchFiles: ['src/index.html'],
-    },
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    watchFiles: ["src/index.html"],
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            title: 'TODO ASHU',
-            filename: 'index.html',
-            inject: 'body',
-        })
-    ]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "Battleship ASHU",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
 };
